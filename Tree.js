fe19ea8219ba2b95632a493d6fc306bb4f67b973
Komal Dhamu <71710063+komalthecoder@users.noreply.github.com>
2020-11-03 16:06:18 +0530

    Add files via upload

---
+++ Tree.js
@@ -0,0 +1,14 @@
+class Tree {
+
+    constructor(x,y,width,height){
+
+        var options = {
+            isStatic:true,
+            restitution:0,
+            friction:1,
+            density:1.2
+        }
+    }
+
+    display();
+}
